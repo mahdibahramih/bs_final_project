8c49014a0843cca81f6c22a50573b9ceee833b62
mahdibahramih <mahdibahramih@gmail.com>
2020-02-04 11:27:04 +0330

    some chane

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from web.models import news , client
 # Register your models here.
+admin.site.register(news)
+admin.site.register(client)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from datetime import datetime
+from django.db import models
 # Create your models here.
+
+
+class news(models.Model):
+    header = models.CharField(max_length=60)
+    text = models.TextField()
+    TTL = models.DateField()
+    def  __str__(self):
+        return "{}  ---  {} ---  {}".format(self.header ,self.text , self.TTL)
+
+
+class client(models.Model):
+    token = models.CharField(max_length=60)
+    place = models.CharField(max_length=100)
+    def  __str__(self):
+        return "{}  ------  {} ".format(self.token ,self.place)
